f5090fcbad19cdfe5e2e02720bca35072a83e4b2
Muhammad Numan Latif <muhammadnumanlatif@gmail.com>
2021-01-24 02:15:23 +0500

    Card and Avatar added

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -12,22 +12,82 @@ class BizCard extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: Stack(
-          alignment: Alignment.center,
+          alignment: Alignment.topCenter,
           children: [
             _getCard(),
+
+            _getAvatar(),
           ],
         ),
       ),
     );
   }
 
+  //GetCard Method
   Container _getCard() {
     return Container(
       width: 350,
-      height: 200,
+      height: 175,
+      margin: EdgeInsets.all(50),
       decoration: BoxDecoration(
         color: Colors.amberAccent.shade700,
-        borderRadius: BorderRadius.circular(4.5),
+        borderRadius: BorderRadius.circular(14.5),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 15,),
+          Text(
+            "Muhammad Numan Latif",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+            ),
+            ),
+          Text(
+            "blueartservices.com",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.person_outline_sharp),
+              Text(
+                "@blueartservices",
+                style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  //GetAvatar Method
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+        border: Border.all(
+          color: Colors.amberAccent,
+          width: 2.0,
+        ),
+        image: DecorationImage(
+          image: NetworkImage("https://picsum.photos/300/300"),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
